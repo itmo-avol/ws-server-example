de9463b9534b63290abe366fac280c7b73031615
Andrey Volynkin <mail.avol@gmail.com>
2019-03-22 22:54:25 +0300

    Добавляет файлы первой реализации

---
+++ client.src/index.ts
@@ -0,0 +1,48 @@
+main();
+
+function main(): void
+{
+	const outputElement = document.querySelector( 'output' );
+	
+	if ( !outputElement )
+	{
+		return;
+	}
+
+	const socket = new WebSocket( 'ws://localhost:8000' );
+
+	socket.addEventListener(
+		'message',
+		( event ) =>
+		{
+			const messages: string[] = JSON.parse( event.data );
+			
+			outputElement.textContent = messages.join( '\n' );
+		}
+	);
+	
+	const formElement = document.querySelector( 'form' );
+	
+	if ( !formElement )
+	{
+		return;
+	}
+	
+	formElement.addEventListener(
+		'submit',
+		( event ) =>
+		{
+			event.preventDefault();
+			
+			const input = formElement.elements.namedItem( 'message' ) as HTMLInputElement | null;
+			
+			if ( !input || !input.value )
+			{
+				return;
+			}
+			
+			socket.send( input.value );
+			input.value = '';
+		}
+	);
+}
---
+++ server.src/index.ts
@@ -0,0 +1,74 @@
+import WebSocket from 'ws';
+import config from './config.json';
+
+const server = new WebSocket.Server(
+	{
+		port: config.webSocketPort,
+	},
+	() =>
+	{
+		const address = server.address();
+		
+		console.log(
+			'WebSocket in listening on port '
+			+ (
+				typeof address === 'string'
+				? address
+				: address.port
+			)
+		);
+	}
+);
+
+const messages: string[] = [];
+
+server.on(
+	'connection',
+	( ws ) =>
+	{
+		ws.on( 'message', onClientMessage );
+		ws.send(
+			JSON.stringify( messages ),
+			onSendError
+		);
+	}
+);
+
+function onClientMessage( this: WebSocket, data: WebSocket.Data ): void
+{
+	if ( typeof data !== 'string' )
+	{
+		this.send(
+			JSON.stringify( {
+				message: 'Wrong data type',
+			} )
+		);
+		
+		return;
+	}
+	
+	messages.push( data );
+	broadcastMessages();
+}
+
+function broadcastMessages(): void
+{
+	for ( const client of server.clients )
+	{
+		if ( client.readyState === WebSocket.OPEN )
+		{
+			client.send(
+				JSON.stringify( messages ),
+				onSendError
+			);
+		}
+	}
+}
+
+function onSendError( error?: Error )
+{
+	if ( error )
+	{
+		console.error( error );
+	}
+}
